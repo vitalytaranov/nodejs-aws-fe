70223a9d42d677309afe8d9812e3f3c4d3f40b1e
Vitaly Taranov <vtarnv@gmail.com>
2020-11-16 23:08:50 +0300

    add task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://f6hqzu6u08.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://f6hqzu6u08.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://f6hqzu6u08.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://jyp7de8r2j.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://f6hqzu6u08.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
